51bf12a9064f390ee21cab987794cf16baa55dff
brenolobao <brenolobao54@gmail.com>
2025-09-27 15:41:29 -0300

    profile-pic path change & script.js add

---
+++ script.js
@@ -0,0 +1,24 @@
+const links = document.querySelectorAll(".navButton");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+        links.forEach(l => l.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
+
+const content = document.querySelector('body');
+const track = document.querySelector('.custom-scrollbar');
+const thumb = document.querySelector('.thumb');
+
+function updateScrollbar() {
+    const ratio = content.scrollTop / (content.scrollHeight - content.clientHeight);
+    const thumbHeight = track.clientHeight * (content.clientHeight / content.scrollHeight);
+
+    thumb.style.height = thumbHeight + 'px';
+    thumb.style.transform = `translateY(${ratio * (track.clientHeight - thumbHeight)}px)`;
+}
+
+content.addEventListener('scroll', updateScrollbar);
+window.addEventListener('resize', updateScrollbar);
+updateScrollbar();
